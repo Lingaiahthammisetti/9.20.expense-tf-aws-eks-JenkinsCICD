9adebef3168d8f09d85d7825c2d79aca759f01ba
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-02-10 18:13:27 +0530

    expense tf aws eks Jenkins CICD

--- 10-sg/variable.tf
+++ 10-sg/variable.tf
@@ -13,35 +13,34 @@ variable "common_tags" {
 }
 variable "db_sg_description" {
   default = "SG for DB Mysql Instances"
-  
 }
 
-variable "vpn_sg_rules" {
-  default = [ 
-     {
-      from_port        = 943
-      to_port          = 943
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    },
-     {
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    },
-     {
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    },
-     {
-      from_port        = 1194
-      to_port          = 1194
-      protocol         = "udp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+# variable "vpn_sg_rules" {
+#   default = [ 
+#      {
+#       from_port        = 943
+#       to_port          = 943
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     },
+#      {
+#       from_port        = 443
+#       to_port          = 443
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     },
+#      {
+#       from_port        = 22
+#       to_port          = 22
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     },
+#      {
+#       from_port        = 1194
+#       to_port          = 1194
+#       protocol         = "udp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     }
+#   ]
  
-}
+# }
